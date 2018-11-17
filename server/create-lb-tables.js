7271e755d23adbf6a752d8956c3f95c5a76fe0a4
graycraft <graycraft@pm.me>
2018-11-17 18:44:58 +0500

    Built-in models migration to PostgreSQL

---
+++ server/create-lb-tables.js
@@ -0,0 +1,14 @@
+/*
+ * @url https://loopback.io/doc/en/lb3/Creating-a-database-schema-from-models#creating-database-tables-for-built-in-models
+ * @url https://loopback.io/doc/en/lb3/Attaching-models-to-data-sources.html
+ */
+'use strict';
+
+var server = require('./server');
+var ds = server.dataSources.postgresql10;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in', ds.adapter.name);
+  ds.disconnect();
+});

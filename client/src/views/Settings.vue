84c5ce5497ab917272c8c4d304ead73b5b700881
graycraft <graycraft@pm.me>
2018-11-21 15:33:34 +0500

    ADAMANT address validation in Vue

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -5,7 +5,9 @@
       <legend>ADAMANT</legend>
       <label>
         ADAMANT address to receive 2FA codes
-        <input autocomplete="on" v-model="account.adamantAddress"/>
+        <input @input="checkAdamantAddress" autocomplete="on" maxlength="21"
+         minlength="21" pattern="U\d+" v-model="account.adamantAddress"/>
+        <div class="note">{{note.adamantAddress}}</div>
       </label>
       <button @click="postAdamantAddress">Get 2FA code</button>
       <br/>
@@ -32,10 +34,22 @@ export default {
     return {
       ...this.$store.state, // Change by commiting a mutation or dispatching an action only
       hotp: null,
-      note: { hotp: null }
+      note: {
+        adamantAddress: null,
+        hotp: null
+      }
     }
   },
   methods: {
+    checkAdamantAddress (e) {
+      const state = e.target.validity
+      if (!state.valid) {
+        // Firefox and IE have no own properties on the ValidityState object
+        this.note.adamantAddress = Object.keys(Object.getPrototypeOf(state)).find(
+          key => state[key]
+        )
+      } else this.note.adamantAddress = ''
+    },
     postAdamantAddress () {
       this.axios.post(this.apiUrl + 'adamantAddress?access_token=' + this.session.id,
         this.account

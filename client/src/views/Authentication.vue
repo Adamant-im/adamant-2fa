016ec15dcb6bb4dd6fe794749c35edec13ab4054
graycraft <graycraft@pm.me>
2018-11-23 13:35:52 +0500

    Authentication page: HOTP validation and verification for accounts with enabled 2FA, mapGetters and mapMutations instead of direct store access, get account data from login response instead of separate request

--- client/src/views/Authentication.vue
+++ client/src/views/Authentication.vue
@@ -7,44 +7,77 @@
     <router-link to="/signup" v-else-if="this.$route.name === 'login' && !session.lastSeen">
       Do not have an account yet? Signup one
     </router-link>
-    <br/>
-    <label>
-      Username
-      <input autocomplete="on" v-model="account.username"/>
-    </label>
-    <br/>
-    <label>
-      Password
-      <input autocomplete="on" type="password" v-model="account.password"/>
-    </label>
-    <br/>
-    <button @click="login" v-if="this.$route.name === 'login'">Login</button>
-    <button @click="signup" v-else-if="this.$route.name === 'signup'">Signup</button>
-    <div class="note">{{note.auth}}</div>
+    <fieldset v-if="session.created && account.se2faEnabled">
+      <label class="block">
+        <input @input="validateHotp" maxlength="6" minlength="6" pattern="\d+"
+          placeholder="2FA code" ref="hotpInput" required v-model="hotp"/>
+      </label>
+      <button @click="verifyHotp" ref="hotpButton">Verify</button>
+      <div class="note">{{note.hotp}}</div>
+    </fieldset>
+    <fieldset v-else>
+      <label class="block">
+        Username
+        <input autocomplete="on" v-model="account.username"/>
+      </label>
+      <label class="block">
+        Password
+        <input autocomplete="on" type="password" v-model="account.password"/>
+      </label>
+      <button @click="login" v-if="this.$route.name === 'login'">Login</button>
+      <button @click="signup" v-else-if="this.$route.name === 'signup'">Signup</button>
+      <div class="note">{{note.auth}}</div>
+    </fieldset>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+
 export default {
+  computed: {
+    ...mapGetters([
+      'account', 'session', 'apiUrl'
+    ])
+  },
   data () {
     return {
-      ...this.$store.state, // Change by commiting a mutation or dispatching an action only
-      note: { auth: null }
+      hotp: null,
+      hotpError: {
+        count: 0,
+        value: null
+      },
+      note: {
+        auth: null,
+        hotp: null
+      }
     }
   },
   methods: {
+    ...mapMutations([
+      'CLEAR_SESSION', 'SET_ACCOUNT', 'SET_SESSION'
+    ]),
     login () {
       this.axios.post(this.apiUrl + 'login',
         this.account
       )
         .then(res => {
           if (res.status === 200) {
-            this.$store.commit('SET_SESSION', {
-              ...res.data,
-              timeDelta: Date.now() - Date.parse(res.data.created)
+            this.SET_SESSION({
+              created: res.data.created,
+              id: res.data.id,
+              lastSeen: res.data.lastSeen,
+              timeDelta: Date.now() - Date.parse(res.data.created),
+              ttl: res.data.ttl
             })
-            this.$store.dispatch('getAccountData')
-            this.$router.push('settings')
+            this.SET_ACCOUNT({
+              adamantAddress: res.data.adamantAddress,
+              id: res.data.userId,
+              se2faEnabled: res.data.se2faEnabled
+            })
+            if (!this.account.se2faEnabled) {
+              this.$router.push('settings')
+            }
             console.info(res)
           } else console.warn(res)
         })
@@ -70,11 +103,69 @@ export default {
           const error = err.response.data.error
           this.note.auth = `${error.statusCode} ${error.message}`
         })
+    },
+    validateHotp (e) {
+      const state = e.target.validity
+      if (!state.valid) {
+        // Firefox and IE have no own properties on the ValidityState object
+        const reason = Object.keys(Object.getPrototypeOf(state)).find(
+          key => state[key]
+        )
+        this.note.hotp = {
+          patternMismatch: 'Code does not match pattern',
+          tooShort: 'Code is too short',
+          valueMissing: 'Code is required to login'
+        }[reason]
+        this.$refs.hotpButton.disabled = true
+      } else {
+        this.note.hotp = ''
+        this.$refs.hotpButton.disabled = false
+      }
+    },
+    verifyHotp () {
+      console.log(this.session.id, this.account.id, this.hotp)
+      this.axios.get(this.apiUrl + 'verifyHotp', {
+        params: {
+          access_token: this.session.id,
+          id: this.account.id,
+          hotp: this.hotp
+        }
+      })
+        .then(res => {
+          if (typeof res.data === 'boolean') {
+            if (res.data) {
+              this.$router.push('settings')
+              // this.note.hotp = ''
+            } else {
+              this.note.hotp = 'Code is not valid, ' + (2 - this.hotpError.count) + ' attempts left'
+              if (this.hotpError.value !== this.hotp) {
+                this.hotpError.count = 1
+                this.hotpError.value = this.hotp
+              } else {
+                this.hotpError.count++
+                if (this.hotpError.count > 2) {
+                  this.axios.post(this.apiUrl + 'logout/?access_token=' + this.session.id)
+                    .then(res => {
+                      if (res.status === 204) {
+                        this.CLEAR_SESSION()
+                        this.$router.push('login')
+                        console.info(res)
+                      } else console.warn(res)
+                    })
+                    .catch(err => console.error(err))
+                }
+              }
+            }
+            console.info(res)
+          } else console.warn(res)
+        })
+        .catch(err => console.error(err))
     }
   }
 }
 </script>
 
 <style scoped>
+.block {display: block}
 .note {color: red}
 </style>

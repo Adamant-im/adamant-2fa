8e221e672e9c59edf66c07b65847197a31c2a5fb
graycraft <graycraft@pm.me>
2018-11-23 13:58:08 +0500

    Loopback: Account model validation, validate HOTP and set se2faEnabled field in verifyHotp endpoint, new disable2fa endpoint, hook for User's remote method "login"

--- common/models/account.js
+++ common/models/account.js
@@ -5,6 +5,7 @@ const methods = require('./account.json').methods;
 
 module.exports = function(Account) {
   Account.adamantAddress = function(adamantAddress, id, next) {
+    // Id must be restricted to owner
     Account.findById(id, (err, account) => {
       if (err) next(err);
       const secret = speakeasy.generateSecret({
@@ -25,27 +26,73 @@ module.exports = function(Account) {
         seSecretUrl: secret.otpauth_url,
       };
       account.updateAttributes(data);
-      next(null, { // @todo Send HOTP to ADAMANT address
+      next(null, {
         adamantAddress,
-        hotp,
-        id,
+        hotp, // @todo Send HOTP to ADAMANT address instead
       });
     });
   };
-  Account.validatesPresenceOf('username', 'password');
-  Account.validatesUniquenessOf('username');
-  Account.verifyHotp = function(hotp, id, next) {
+
+  Account.disable2fa = function(id, next) {
+    // Id must be restricted to owner
     Account.findById(id, (err, account) => {
       if (err) next(err);
-      const verified = speakeasy.hotp.verify({
-        counter: account.seCounter,
-        encoding: 'ascii',
-        secret: account.seSecretAscii,
-        token: hotp,
+      account.updateAttributes({
+        se2faEnabled: false,
       });
+      next(null, false);
+    });
+  };
+
+  Account.verifyHotp = function(hotp, id, next) {
+    // Id must be restricted to owner
+    Account.findById(id, (err, account) => {
+      if (err) next(err);
+      let verified;
+      if (/\d{6}/.test(hotp)) {
+        verified = speakeasy.hotp.verify({
+          counter: account.seCounter,
+          encoding: 'ascii',
+          secret: account.seSecretAscii,
+          token: hotp,
+        });
+        account.updateAttributes({
+          se2faEnabled: verified,
+        });
+      } else verified = false;
+      // @todo Send new HOTP to ADAMANT address
       next(null, verified);
     });
   };
+
+  Account.afterRemote('login', function(ctx, output, next) {
+    Account.findById(output.userId, (err, account) => {
+      if (err) next(err);
+      output.setAttribute('adamantAddress', account.adamantAddress);
+      output.setAttribute('se2faEnabled', account.se2faEnabled);
+      next(null, output);
+    });
+  });
+
   Account.remoteMethod('adamantAddress', methods.adamantAddress);
   Account.remoteMethod('verifyHotp', methods.verifyHotp);
+
+  Account.validatesExclusionOf('username', {
+    in: ['admin'],
+    message: 'This username not allowed',
+  });
+  Account.validatesFormatOf('adamantAddress', {
+    allowNull: true,
+    message: 'Address does not match pattern',
+    with: /^U\d/,
+  });
+  Account.validatesLengthOf('adamantAddress', {
+    allowNull: true,
+    message: {is: 'Address is too short or too long'},
+    is: 21,
+  });
+  Account.validatesPresenceOf('username', 'password');
+  Account.validatesUniquenessOf('username', {
+    message: 'User already exists',
+  });
 };

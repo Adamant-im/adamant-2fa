843540aa5a1710205214f6d796414efd3e483499
Dmitrij Papulovskij <graycraft@pm.me>
2018-12-10 00:32:58 +0500

    LanguageSwitcher and SnackbarNote components

---
+++ client/src/components/LanguageSwitcher.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-menu offset-y>
+    <v-btn class="ma-0" flat slot="activator">
+      {{this.$i18n.messages[this.$i18n.locale].name}}
+      <v-icon right>mdi-chevron-down</v-icon>
+    </v-btn>
+    <v-list>
+      <v-list-tile :key="locale" @click="$i18n.locale = locale"
+        v-for="(language, locale) in this.$i18n.messages">
+        <v-list-tile-title>{{language.name}}</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
+</template>
---
+++ client/src/components/SnackbarNote.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-snackbar :timeout="timeout" bottom v-model="value">
+    {{$t(this.text)}}
+  </v-snackbar>
+  <!--v-snackbar :timeout="timeout" bottom v-model="value" v-t="this.text"></v-snackbar-->
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      timeout: 1.5e9,
+      value: true
+    }
+  },
+  props: ['text']
+}
+</script>

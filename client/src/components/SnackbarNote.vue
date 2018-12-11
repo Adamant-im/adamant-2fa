15e538aed24c03b136fa541c278a592a79d92757
Dmitrij Papulovskij <graycraft@pm.me>
2018-12-12 00:21:09 +0500

    i18n object syntax bug fix

--- client/src/components/SnackbarNote.vue
+++ client/src/components/SnackbarNote.vue
@@ -1,18 +1,28 @@
 <template>
-  <v-snackbar :timeout="timeout" bottom v-model="value">
-    {{$t(this.text)}}
+  <v-snackbar :timeout="timeout" top v-model="value">
+    {{ $t(this.path, this.args) }}
   </v-snackbar>
-  <!--v-snackbar :timeout="timeout" bottom v-model="value" v-t="this.text"></v-snackbar-->
 </template>
 
 <script>
 export default {
+  computed: {
+    args () {
+      console.log(this.options)
+      return typeof this.options === 'string' ? null : this.options.args
+    },
+    path () {
+      console.log(this.options)
+      return typeof this.options === 'string' ? this.options : this.options.path
+    }
+  },
   data () {
+    console.log(this)
     return {
-      timeout: 1.5e9,
+      timeout: 1500,
       value: true
     }
   },
-  props: ['text']
+  props: ['options']
 }
 </script>

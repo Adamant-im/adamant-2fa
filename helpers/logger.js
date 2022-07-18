780faa3ba218f9c0d9d62ba0c0c835c623959349
confleux <confleux@gmail.com>
2022-07-18 13:24:16 +0300

    chore: updated logger

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,4 +1,14 @@
 'use strict';
+const fs = require('fs');
+if (!fs.existsSync('./logs')) {
+  fs.mkdirSync('./logs');
+}
+
+const infoStr = fs.createWriteStream(`./logs/${date()}.log`, {
+  flags: 'a',
+});
+
+infoStr.write(`\n\n[The bot started] _________________${fullTime()}_________________\n`);
 
 const logger = {
   errorLevel: 'log',
@@ -15,24 +25,61 @@ const logger = {
   },
   error(str) {
     if (['error', 'warn', 'info', 'log'].includes(this.errorLevel)) {
-      this.logger.error(str);
+      this.logger.log('\x1b[31m', 'error|' + fullTime(), '\x1b[0m', str);
+      infoStr.write(`\n ` + 'error|' + fullTime() + '|' + str);
     }
   },
   warn(str) {
     if (['warn', 'info', 'log'].includes(this.errorLevel)) {
-      this.logger.warn(str);
+      this.logger.log('\x1b[33m', 'warn|' + fullTime(), '\x1b[0m', str);
+      infoStr.write(`\n ` + 'warn|' + fullTime() + '|' + str);
     }
   },
   info(str) {
     if (['info', 'log'].includes(this.errorLevel)) {
-      this.logger.info(str);
+      this.logger.log('\x1b[32m', 'info|' + fullTime(), '\x1b[0m', str);
+      infoStr.write(`\n ` + 'info|' + fullTime() + '|' + str);
     }
   },
   log(str) {
     if (this.errorLevel === 'log') {
-      this.logger.log(str);
+      this.logger.log('\x1b[34m', 'log|' + fullTime(), '\x1b[0m', str);
+      infoStr.write(`\n ` + 'log|[' + fullTime() + '|' + str);
     }
   },
 };
+
+function time() {
+  return formatDate(Date.now()).hh_mm_ss;
+}
+
+function date() {
+  return formatDate(Date.now()).YYYY_MM_DD;
+}
+
+function fullTime() {
+  return date() + ' ' + time();
+}
+
+/**
+ * Formats unix timestamp to string
+ * @param {number} timestamp Timestamp to format
+ * @return {object} Contains different formatted strings
+ */
+function formatDate(timestamp) {
+  if (!timestamp) return false;
+  const formattedDate = {};
+  const dateObject = new Date(timestamp);
+  formattedDate.year = dateObject.getFullYear();
+  formattedDate.month = ('0' + (dateObject.getMonth() + 1)).slice(-2);
+  formattedDate.date = ('0' + dateObject.getDate()).slice(-2);
+  formattedDate.hours = ('0' + dateObject.getHours()).slice(-2);
+  formattedDate.minutes = ('0' + dateObject.getMinutes()).slice(-2);
+  formattedDate.seconds = ('0' + dateObject.getSeconds()).slice(-2);
+  formattedDate.YYYY_MM_DD = formattedDate.year + '-' + formattedDate.month + '-' + formattedDate.date;
+  formattedDate.YYYY_MM_DD_hh_mm = formattedDate.year + '-' + formattedDate.month + '-' + formattedDate.date + ' ' + formattedDate.hours + ':' + formattedDate.minutes;
+  formattedDate.hh_mm_ss = formattedDate.hours + ':' + formattedDate.minutes + ':' + formattedDate.seconds;
+  return formattedDate;
+}
 
 module.exports = logger;

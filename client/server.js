32c0fe5a12e31942a39636081874a170cf82c19c
confleux <confleux@gmail.com>
2022-12-12 15:35:07 +0300

    refactor: use new serve server

---
+++ client/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const history = require('connect-history-api-fallback')
+const path = require('path')
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(history())
+
+app.use(express.static(path.join(__dirname, '/dist')))
+
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(port)
+console.log(`Server started on port ${port}`)
